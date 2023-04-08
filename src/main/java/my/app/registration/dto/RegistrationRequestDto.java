fc98e968140fd288636188c17f180782c55045b1
Piotr Kusibab <piotr.kusibab@amway.com>
2023-04-08 14:34:31 +0200

    added complex validation for password

--- src/main/java/my/app/registration/dto/RegistrationRequestDto.java
+++ src/main/java/my/app/registration/dto/RegistrationRequestDto.java
@@ -4,12 +4,16 @@ import lombok.Builder;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 @Builder
 public record RegistrationRequestDto(
         @Email(regexp = "^(.+)@(\\S+)$", message = "You have to provide right email format!")
-        @NotNull String email,
-        @NotNull @Size(min = 6, message = "Your password should contains at least 6 signs!") String password,
-        @NotNull String repeatPassword
+        @NotNull
+        String email,
+        @NotNull
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{6,}$", message = "Password must be at least 6 characters long and contain at least 1 digit, 1 lowercase letter, 1 uppercase letter, 1 special character, and no whitespaces.")
+        String password,
+        @NotNull
+        String repeatPassword
 ) {}
